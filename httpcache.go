303dedb729fdbfd38791d42784c356d41d140daa
Ferocious Bite <ferocious.bite.eve@gmail.com>
2021-08-25 16:23:42 +0300

    remove caching read closer

--- httpcache.go
+++ httpcache.go
@@ -299,18 +299,9 @@ func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
 			}
 			resp.Body.Close()
 			resp.Body = ioutil.NopCloser(bodyBuffer)
-			// Delay caching until EOF is reached.
-			//resp.Body = &cachingReadCloser{
-			//	R: resp.Body,
-			//	OnEOF: func(r io.Reader) {
-			//		resp := *resp
-			//		resp.Body = ioutil.NopCloser(r)
-			rc := *resp
-			respBytes, err := httputil.DumpResponse(&rc, true)
+			respBytes, err := httputil.DumpResponse(resp, true)
 			if err == nil {
 				t.Cache.Set(cacheKey, respBytes)
-				//	}
-				//},
 			}
 		default:
 			respBytes, err := httputil.DumpResponse(resp, true)
@@ -570,33 +561,4 @@ func headerAllCommaSepValues(headers http.Header, name string) []string {
 		vals = append(vals, fields...)
 	}
 	return vals
-}
-
-// cachingReadCloser is a wrapper around ReadCloser R that calls OnEOF
-// handler with a full copy of the content read from R when EOF is
-// reached.
-type cachingReadCloser struct {
-	// Underlying ReadCloser.
-	R io.ReadCloser
-	// OnEOF is called with a copy of the content of R when EOF is reached.
-	OnEOF func(io.Reader)
-
-	buf bytes.Buffer // buf stores a copy of the content of R.
-}
-
-// Read reads the next len(p) bytes from R or until R is drained. The
-// return value n is the number of bytes read. If R has no data to
-// return, err is io.EOF and OnEOF is called with a full copy of what
-// has been read so far.
-func (r *cachingReadCloser) Read(p []byte) (n int, err error) {
-	n, err = r.R.Read(p)
-	r.buf.Write(p[:n])
-	if errors.Is(err, io.EOF) {
-		r.OnEOF(bytes.NewReader(r.buf.Bytes()))
-	}
-	return n, err
-}
-
-func (r *cachingReadCloser) Close() error {
-	return r.R.Close()
 }
